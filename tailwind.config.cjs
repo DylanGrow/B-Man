5d7a668687b4f2213b9f006b7d95dc3892b8adc6
Skylar Grow <dylangrow@gmail.com>
2026-07-26 18:21:46 -0400

    Add Tailwind CSS configuration file

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["system-ui", "-apple-system", "BlinkMacSystemFont", "Segoe UI", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
